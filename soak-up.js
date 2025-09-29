ae75df6f7c972bb08b74082b9b033bab602e9895
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-29 06:37:23 -0400

    Update README.md

--- soak-up.js
+++ soak-up.js
@@ -3,7 +3,7 @@ import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
 import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP, AllProps, AP} from './ts-refs/soak-up/types' */;
+/** @import {Actions, PAP, AllProps, AP, BAP, PropMap} from './ts-refs/soak-up/types' */;
 
 /**
  * @implements {Actions}
@@ -20,9 +20,49 @@ class SoakUp extends BE {
             fullyParsed: {},
         },
         positractions: [resolved, rejected],
+        compacts:{
+            when_soakUpRules_changes_call_fullyParse: 0,
+        }
     };
 
     de = de;
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    fullyParse(self){
+        const {soakUpRules} = self;
+        for(const rule of soakUpRules){
+            const {propMap} = rule;
+            /** @type {PropMap[]} */
+            const parsedPropMap = [];
+            const commaSplit = propMap.split(',').map(s => s.trim()).filter(s => s.length > 0);
+            for(const part of commaSplit){
+                //const match = part.match(/^(?<propMap>.*) as (?<asProp>.*)$/); 
+                const [srcProp, destProp] = part.split(' as ').map(s => s.trim());
+                parsedPropMap.push({
+                    srcProp,
+                    destProp
+                });
+            }
+        }
+        return /** @type {PAP} */ ({
+            fullyParsed: true
+        });
+    }
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async hydrate(self){
+        return /** @type {PAP} */ ({
+            resolved: true
+        });
+    }
 }
 
 await SoakUp.bootUp();
